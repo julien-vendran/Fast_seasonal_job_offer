39ef5f267e0a313ca1391877396d34627addae7a
CecileDestIG <cecile.destaing@etu.umontpellier.fr>
2022-12-01 11:59:46 +0100

    job number

--- src/main/java/com/polytech/offer/controllers/OfferController.java
+++ src/main/java/com/polytech/offer/controllers/OfferController.java
@@ -1,40 +1,40 @@
 package com.polytech.offer.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.polytech.offer.entity.OfferEntity;
 import com.polytech.offer.services.OfferService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/offer")
 @RestController
+@CrossOrigin
 public class OfferController {
 
     private final OfferService offerService;
-    private final ObjectMapper objectMapper;
 
-    @GetMapping
-    public Iterable<OfferEntity> getAll() {
+    @GetMapping()
+    public Iterable<OfferEntity> getAll(){
         return offerService.getAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<OfferEntity> getById(@PathVariable Long id) {
+    public Optional<OfferEntity> getById(@PathVariable Long id){
         return offerService.getById(id);
     }
-
-    @PostMapping
-    public OfferEntity createOrUpdate(@RequestBody OfferEntity offerEntity) {
+    @PostMapping()
+    public OfferEntity createOrUpdate(@RequestBody OfferEntity offerEntity){
         return offerService.createOrUpdate(offerEntity);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id){
         offerService.delete(id);
     }
 }
--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,48 +1,84 @@
 DROP DATABASE IF EXISTS fast_seasonal;
-CREATE DATABASE fast_seasonal;
 
-DROP TABLE IF EXISTS offer;
-CREATE TABLE offer
+DROP TABLE IF EXISTS cv;
+CREATE TABLE cv
 (
-    idOffer serial PRIMARY KEY,
-    title  VARCHAR(100),
-    description VARCHAR(1000),
-    publishDay DATE,
-    jobStartingDate DATE,
-    location VARCHAR(100),
-    jobnumber INTEGER,
-    salary FLOAT,
-    advantages VARCHAR(500),
-    job VARCHAR(100),
-    zones VARCHAR(100),
-    author VARCHAR(50),
-    keywords VARCHAR(100)
+    id           SERIAL PRIMARY KEY,
+    name         VARCHAR,
+    content_type VARCHAR,
+    size         BIGINT,
+    data         OID
 );
 
 DROP TABLE IF EXISTS jobseeker;
 CREATE TABLE jobseeker
 (
-    id        serial PRIMARY KEY,
+    id        SERIAL PRIMARY KEY,
+    cv_id     BIGINT,
     lastname  VARCHAR(100),
     firstname VARCHAR(100),
     city      VARCHAR(100),
+    region    VARCHAR(100),
     job       VARCHAR(100),
     phone     VARCHAR(20),
-    email     VARCHAR(100)
+    email     VARCHAR(100),
+    keywords  VARCHAR(500), -- Split on ","
+    FOREIGN KEY (cv_id) REFERENCES cv (id)
 );
 
 DROP TABLE IF EXISTS recruiter;
 CREATE TABLE recruiter
 (
-    id          serial PRIMARY KEY,
-    lastname    VARCHAR(100),
-    firstname   VARCHAR(100),
-    companyName VARCHAR(100),
-    city        VARCHAR(100),
-    job         VARCHAR(100),
-    phone       VARCHAR(20),
-    email       VARCHAR(100)
+    id           SERIAL PRIMARY KEY,
+    lastname     VARCHAR(100),
+    firstname    VARCHAR(100),
+    company_name VARCHAR(100),
+    city         VARCHAR(100),
+    job          VARCHAR(100),
+    phone        VARCHAR(20),
+    email        VARCHAR(100)
 );
 
+DROP TABLE IF EXISTS grade;
+CREATE TABLE grade
+(
+    recruiter_id BIGINT,
+    jobseeker_id BIGINT,
+    mark      INTEGER,
+    comment   VARCHAR(200),
+    PRIMARY KEY (recruiter_id, jobseeker_id),
+    FOREIGN KEY (recruiter_id) REFERENCES recruiter (id),
+    FOREIGN KEY (jobseeker_id) REFERENCES jobseeker (id)
+);
+
+DROP TABLE IF EXISTS offer CASCADE;
+CREATE TABLE offer
+(
+    id                SERIAL PRIMARY KEY,
+    title             VARCHAR(100),
+    description       VARCHAR(1000),
+    publish_day       DATE,
+    job_starting_date DATE,
+    city              VARCHAR(100),
+    region            VARCHAR(100),
+    job_number         INTEGER,
+    salary            FLOAT,
+    advantages        VARCHAR(500),
+    job               VARCHAR(100),
+    zones             VARCHAR(100),
+    author            VARCHAR(50),
+    keywords          VARCHAR(500)
+);
+
+DROP TABLE IF EXISTS jobseeker_offer;
+CREATE TABLE jobseeker_offer
+(
+    jobseeker_id BIGINT,
+    offer_id     BIGINT,
+    FOREIGN KEY (jobseeker_id) REFERENCES jobseeker (id),
+    FOREIGN KEY (offer_id) REFERENCES offer (id),
+    PRIMARY KEY (jobseeker_id, offer_id)
+
+);
 
 CREATE SEQUENCE hibernate_sequence START 1;
