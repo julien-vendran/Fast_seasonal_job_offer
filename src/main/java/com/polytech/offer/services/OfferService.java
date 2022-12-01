46df04bcb4e531eee905d50d9f4a5af70e9341d3
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2022-12-01 17:39:59 +0100

    feat: send message to kafka when creating a new offer

--- src/main/java/com/polytech/offer/services/OfferService.java
+++ src/main/java/com/polytech/offer/services/OfferService.java
@@ -1,10 +1,13 @@
 package com.polytech.offer.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.polytech.offer.entity.JobSeekerEntity;
 import com.polytech.offer.entity.OfferEntity;
 import com.polytech.offer.kafka.KafkaSenderMsg;
 import com.polytech.offer.repository.OfferRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,26 +17,33 @@ import java.util.Optional;
 public class OfferService {
 
     private final OfferRepository offerRepository;
-    private final KafkaSenderMsg msgKafka;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    private final static String KAFKA_OFFER_TOPIC_NAME = "addOffer";
 
-    public Iterable<OfferEntity> getAll(){
+    public Iterable<OfferEntity> getAll() {
         return offerRepository.findAll();
     }
 
-    public Optional<OfferEntity> getById(Long offerId){
+    public Optional<OfferEntity> getById(Long offerId) {
         return offerRepository.findById(offerId);
     }
 
-    public OfferEntity createOrUpdate(OfferEntity offerEntity){
-        msgKafka.sendMsg("Offer as been registered in BD","msg_topic");
-        return offerRepository.save(offerEntity);
+    public OfferEntity createOrUpdate(OfferEntity offerEntity) {
+        OfferEntity savedOffer = offerRepository.save(offerEntity);
+        try {
+            kafkaTemplate.send(KAFKA_OFFER_TOPIC_NAME, objectMapper.writeValueAsString(savedOffer));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return savedOffer;
     }
 
     public Iterable<OfferEntity> getByAuthor(Long id){
         return offerRepository.findByAuthor(id);
     }
 
-    public void delete(Long offerId){
+    public void delete(Long offerId) {
         offerRepository.deleteById(offerId);
     }
 
